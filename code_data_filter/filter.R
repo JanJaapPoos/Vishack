f9f09dd52d3d494e86a6e9761a836b6c66785847
Jan Jaap Poos <janjaap.poos@wur.nl>
2018-03-09 16:17:08 +0100

    added data filter and filtered data (BTS)

---
+++ code_data_filter/filter.R
@@ -0,0 +1,12 @@
+setwd("d:\\")
+
+
+rawdat <- read.csv("CPUE per length per Hour and Swept Area_2018-03-08 14_15_14.csv")
+names(rawdat)
+nrow(rawdat)
+fdat <- rawdat[rawdat$AphiaID %in% c(127160,127142,105883),]
+nrow(fdat)
+fdat <- fdat[fdat$ICESArea %in% c("IVa","IVb","IVc"),]
+nrow(fdat)
+write.csv(fdat,"CPUE per length per Hour and Swept Area_2018-03-08 14_15_14_filtered.csv")
+
