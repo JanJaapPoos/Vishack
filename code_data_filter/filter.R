88df97a8632c94597bce8028597273cb9be5d6a6
Jan Jaap Poos <janjaap.poos@wur.nl>
2018-03-09 16:44:43 +0100

    fixed bug in data filter (and filtered data)

--- code_data_filter/filter.R
+++ code_data_filter/filter.R
@@ -4,7 +4,7 @@ setwd("d:\\")
 rawdat <- read.csv("CPUE per length per Hour and Swept Area_2018-03-08 14_15_14.csv")
 names(rawdat)
 nrow(rawdat)
-fdat <- rawdat[rawdat$AphiaID %in% c(127160,127142,105883),]
+fdat <- rawdat[rawdat$AphiaID %in% c(127160,127143,105883),]
 nrow(fdat)
 fdat <- fdat[fdat$ICESArea %in% c("IVa","IVb","IVc"),]
 nrow(fdat)
