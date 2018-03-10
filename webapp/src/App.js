3cd35fc88943e491131951fd82f404a238b6113d
Hanne Nijhuis <prostic@gmail.com>
2018-03-10 11:12:40 +0100

    Add Firebase

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,7 +2,44 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import Leaflet from './Leaflet'
 import Login from './components/Login'
+import * as firebase from 'firebase'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+const config = {
+  apiKey: "AIzaSyDh89laiSSyRsJkGMVoZ8C_QQ0Ekgt4WpA",
+  authDomain: "vishack-d2af3.firebaseapp.com",
+  databaseURL: "https://vishack-d2af3.firebaseio.com",
+  projectId: "vishack-d2af3",
+  storageBucket: "vishack-d2af3.appspot.com",
+  messagingSenderId: "244277080689"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey("BASTSsT5Cz7XnyYNArw_YhtR3QRruks16Hc8hEPGP6n5yVcOj45z4TTsuAWRWZBhxtEao2ovwDaV9csbm4e_O3U");
+messaging.requestPermission()
+.then(function() {
+  console.log('Notification permission granted.');
+  messaging.getToken()
+  .then(function(currentToken) {
+    if (currentToken) {
+//      sendTokenToServer(currentToken);
+//      updateUIForPushEnabled(currentToken);
+    } else {
+      // Show permission request.
+      console.log('No Instance ID token available. Request permission to generate one.');
+    }
+  })
+  .catch(function(err) {
+    console.log('An error occurred while retrieving token. ', err);
+  });
+ })
+.catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
 
 class App extends Component {
   state = {
