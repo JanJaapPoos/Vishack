a679ea36ace83a5845db0a163689de58a2fb27b1
Lucas van der Meer <lucasvandermeer@gmail.com>
2018-03-10 10:28:11 +0100

    fix add trek name bug

--- webapp/src/components/Share.js
+++ webapp/src/components/Share.js
@@ -4,8 +4,8 @@ import image from '../assets/login-form.jpg'
 export default class Login extends React.Component {
   render() {
     return (
-      <div class="add-trek">
-        <h1>Voeg een trek toe</h1>
+      <div className="add-trek">
+        <h1>Deel trekgegevens</h1>
         <div className="choice-button">
           <i className="material-icons">supervisor_account</i>
           <h4>Deel eLogboek met NVWA</h4>
